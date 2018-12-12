6f2081b27229ad7c17aafd1f529c16d6d6dfc104
adligeerik <erik.von.ke@gmail.com>
2018-12-12 22:51:56 +0100

    chromoter works

--- app/src/main/java/com/example/erikv/timerlogger/StartActivity.java
+++ app/src/main/java/com/example/erikv/timerlogger/StartActivity.java
@@ -2,6 +2,7 @@ package com.example.erikv.timerlogger;
 
 import android.content.Intent;
 import android.os.CountDownTimer;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,40 +11,45 @@ import android.widget.Chronometer;
 import android.widget.TextView;
 
 import static android.provider.AlarmClock.EXTRA_MESSAGE;
+import static java.lang.Boolean.FALSE;
+import static java.lang.Boolean.TRUE;
 
 public class StartActivity extends AppCompatActivity {
 
     Chronometer simpleChronometer;
     TextView timerdisplay;
     Button startbutton;
-    public int counter;
+    boolean iscounting = FALSE;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        initparams();
+    }
+
+    private void initparams(){
         timerdisplay = (TextView)findViewById(R.id.textView);
         startbutton = (Button)findViewById(R.id.startbutton);
         simpleChronometer = (Chronometer) findViewById(R.id.counter); // initiate a chronometer
-
-        //startbutton.setOnClickListener(this);
+        //simpleChronometer.setFormat("Time Running - %s");
+        simpleChronometer.stop();
     }
-
 
 
     /** Called when the user taps the Start activity button */
     public void startTiming(View view) {
-        String msg = "test";
-        timerdisplay.setText(msg);
-        new CountDownTimer(30000, 1000){
-            public void onTick(long millisUntilFinished){
-                timerdisplay.setText(String.valueOf(counter));
-                counter++;
-            }
-            public  void onFinish(){
-                timerdisplay.setText("FINISH!!");
-            }
-        }.start();
+        if (!iscounting){
+            simpleChronometer.setBase(SystemClock.elapsedRealtime());
+            simpleChronometer.start();
+            iscounting = TRUE;
+        }
+        else if(iscounting){
+            simpleChronometer.stop();
+            iscounting = FALSE;
+        }
     }
 
 }
