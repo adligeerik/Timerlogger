0bc80f284617040745f661b56004816c8ba95b2d
adligeerik <erik.von.ke@gmail.com>
2018-12-12 22:27:45 +0100

    countdown works

--- app/src/main/java/com/example/erikv/timerlogger/StartActivity.java
+++ app/src/main/java/com/example/erikv/timerlogger/StartActivity.java
@@ -1,18 +1,22 @@
 package com.example.erikv.timerlogger;
 
 import android.content.Intent;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Chronometer;
 import android.widget.TextView;
 
 import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class StartActivity extends AppCompatActivity {
 
+    Chronometer simpleChronometer;
     TextView timerdisplay;
     Button startbutton;
+    public int counter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +24,8 @@ public class StartActivity extends AppCompatActivity {
         setContentView(R.layout.activity_start);
         timerdisplay = (TextView)findViewById(R.id.textView);
         startbutton = (Button)findViewById(R.id.startbutton);
+        simpleChronometer = (Chronometer) findViewById(R.id.counter); // initiate a chronometer
+
         //startbutton.setOnClickListener(this);
     }
 
@@ -27,12 +33,17 @@ public class StartActivity extends AppCompatActivity {
 
     /** Called when the user taps the Start activity button */
     public void startTiming(View view) {
-        //Intent intent = new Intent(this, DisplayMessageActivity.class);
-        //TextView textview = (TextView)findViewById(R.id.textView);
         String msg = "test";
         timerdisplay.setText(msg);
-        //intent.putExtra(EXTRA_MESSAGE, message);
-        //startActivity(intent);
+        new CountDownTimer(30000, 1000){
+            public void onTick(long millisUntilFinished){
+                timerdisplay.setText(String.valueOf(counter));
+                counter++;
+            }
+            public  void onFinish(){
+                timerdisplay.setText("FINISH!!");
+            }
+        }.start();
     }
 
 }
