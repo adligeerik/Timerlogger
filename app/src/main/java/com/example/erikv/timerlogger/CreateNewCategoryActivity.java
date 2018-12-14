fe6ce89393e7ddf6ce6510c5fa17a50b40fe4cdd
adligeerik <erik.von.ke@gmail.com>
2018-12-14 22:02:08 +0100

    save file started

--- app/src/main/java/com/example/erikv/timerlogger/CreateNewCategoryActivity.java
+++ app/src/main/java/com/example/erikv/timerlogger/CreateNewCategoryActivity.java
@@ -2,12 +2,34 @@ package com.example.erikv.timerlogger;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import java.io.FileNotFoundException;
+
+import static java.lang.Boolean.FALSE;
+import static java.lang.Boolean.TRUE;
 
 public class CreateNewCategoryActivity extends AppCompatActivity {
 
+    String fileName = "categories";
+    Boolean isFile = FALSE;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_new_category);
+    }
+
+
+    public void addCategory(View view){
+
+        if (!isFile){
+            try {
+                openFileInput(fileName);
+                isFile = TRUE;
+            } catch (FileNotFoundException e) {
+                
+            }
+        }
+
     }
 }
